b4e85b7916ed30d3cf5f0f3a63ef0d748b797ff9
goodmeow <harunbam3@gmail.com>
2020-10-11 16:57:59 +0700

    path: also allow both ld linker too
    Signed-off-by: goodmeow <harunbam3@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -102,8 +102,10 @@ var Configuration = map[string]PathConfig{
 	// Kernel tools (VDSO)
 	"aarch64-linux-android-ar":    Allowed,
 	"aarch64-linux-android-as":    Allowed,
+	"aarch64-linux-android-ld":    Allowed,
 	"arm-linux-androidkernel-ar":  Allowed,
 	"arm-linux-androidkernel-as":  Allowed,
+	"arm-linux-androidkernel-ld":  Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
